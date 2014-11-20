4f78b29a81db2a7c7ac7f5b0ba177920b99598ce
Victor Yacovlev <v.yacovlev@gmail.com>
2014-11-20 15:18:17 +0300

    Code review first iteration

--- footballer_dz2.cpp
+++ footballer_dz2.cpp
@@ -1,24 +1,62 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+/*= Ошибка компиляции: такой файл не существует
 #include "lib2dz.h"
+*/
+#include "footballer_dz2.h" 
 
+/*=
+ * 1. Если функция используется только в данном .cpp файле, то желательно
+ * в начале дописать ключевое слово static (это автоматически подразумевается
+ * компилятором, но явное указание улучшает читаемость кода)
+ * 2. Если функция является общедоступной, то ее заголовок должен быть
+ * в .h, а не .cpp файле
+ */
 void read(std::vector< int > *sorted_array, int number_of_elements);
 
 using std::cin;
 using std::cout;
 
+/*= Отступы !!!
+ * Общепринятый стандарт: 1 отступ = 4 пробела,
+ * должны быть внутри каждого блока {}
+ */
 int main()
 {
+    /*=
+     * 1. Объявление каждой переменной должно быть отдельным statement'ом
+     * с новой строки, поскольку в будущем возможны изменения типов, а
+     * системы контроля версий ориентированы на построчные изменения текстов
+     * 2. ВСЕ переменные нужно инициализировать начальными значениями
+     * 3. Число элементов и индекс -- это не int, а беззнаковый тип size_t.
+     * См. reference класса std::vector
+     */
 int number_of_elements = 0, index = 0;
 int left_answer, right_answer, right = 0, right_new, first, second;
 double sum = 0, full_sum = 0, answer = 0, par = 0;
 
 cin >> number_of_elements;
 
+    /*=
+     * Пробелы внутри <> не ставятся (за исключением вложенных треугольных скобок).
+     * См. code style guide
+     */
 std::vector< int > array_with_indexes(number_of_elements);
 std::vector< int > sorted_array(number_of_elements);;
+    /*=
+     * Имена большими буквами приняты для констант или макросов.
+     * См. code style guide
+     */
 std::vector< double > SUM(number_of_elements);
 
+/*=
+ * 1. Составные типы данных в C++ передаются по ссылке, а не через указатель,
+ * единственное исключение -- нулевой указатель.
+ * Аналогичное замечание -- при вызове quicksort_indexes.
+ * 2. Число элементов передавать не нужно, оно однозначно определяется
+ * через vector::size()
+ * 3. Более логично строку cin >> number_of_elements внести внутрь функции read
+ */
 read(&sorted_array, number_of_elements);
 
 for (index = 0; index < number_of_elements; ++index) {
@@ -27,21 +65,37 @@ for (index = 0; index < number_of_elements; ++index) {
 
 quicksort_indexes(&sorted_array, 0, number_of_elements - 1, &array_with_indexes);
 
-
+/*= Все манипуляции с std::cout должны выполняться непосредственно перед выводом */
 std::cout.precision(15);
 
+/*=
+ * 1. Программа на этой строке всегда падает независимо от входных данных. Подумай, почему.
+ * 2. Падение не очевидно для отладчика, поскольку используются квадратные скобки всесто
+ * метода vector::at(size_t index)
+ * 3. Что будет, если 0==sorted_array.size() ?
+ */
 sorted_array[number_of_elements] = sorted_array[number_of_elements - 1];
 
+/*=
+ * 1. Весь код ниже, до вывода на экран, должен быть вынесен в отдельную функцию
+ * с понятным именем, которое описывает, что этот код делает
+ * 2. Используется очень опасный прием: внутри цикла for меняются значения переменных, объявленных
+ * вне этого цикла. C++, в отличии от Си, позволяет объявлять переменные в любом месте
+ */
 for (index = number_of_elements - 1, right = number_of_elements - 1; 
                                                          index >= 0; --index) {
 
 full_sum += sorted_array[index];
 SUM[index] = full_sum;
 
+/*= Присваивания -- отдельными инструкциями, разделенными ;, а не запятой ! */
 first = sorted_array[index],
+        /*= Что будет, если (index + 1) == sorted_array().size() ? */
 second = sorted_array[index + 1],
+
+/*= Зачем здесь double??? В данной задаче используется исключительно целочисленная арифметика */
 par = static_cast<double>(first) + static_cast<double>(second);  
-  if (par < sorted_array[right]) {  
+  if (par < sorted_array[right]) {
     if (answer < sum) {
       answer = sum;
       left_answer = index + 1;
@@ -52,12 +106,12 @@ par = static_cast<double>(first) + static_cast<double>(second);
 
     if (answer >= sorted_array[right_new] * (right_new)) break;
 
-    sum -= SUM[right_new + 1] - SUM[right] + sorted_array[right]; 
+    sum -= SUM[right_new + 1] - SUM[right] + sorted_array[right];
 
     right = right_new;
   }
 
-sum = sum + static_cast<double>(sorted_array[index]); 
+sum = sum + static_cast<double>(sorted_array[index]);
 }
 
     if (answer < sum) {
@@ -66,11 +120,11 @@ sum = sum + static_cast<double>(sorted_array[index]);
       right_answer = right;
     }
 
-
+/*= Лучше выводить std::endl вместо платформо-зависимой ESC-последовательности "\n" */
 cout << answer << "\n";
 quicksort(&array_with_indexes, left_answer, right_answer);
 
-
+/*= Три строки ниже можно заменить всего одной строкой, используя стандартную библиотеку STL */
 for (index = left_answer; index <= right_answer; ++index) {
       cout << array_with_indexes[index] + 1 << " "; 
     }
@@ -80,6 +134,9 @@ return 0;
 
   void read(std::vector< int > *sorted_array, int number_of_elements)
   { 
+/*= Три строки ниже можно заменить всего одной строкой, используя стандартную библиотеку STL
+ * (в этом случае функция read тогда вообще не нужна)
+*/
     for (int index = 0; index < number_of_elements; ++index) {
       std::cin >> (*sorted_array)[index];
     }
--- footballer_dz2.h
+++ footballer_dz2.h
@@ -1,6 +1,29 @@
+﻿/*=
+1. В header файле обязательно нужен Header Guard вида
+#ifndef FOOTBALLER_DZ2_H
+#define FOOTBALLER_DZ2_H
+ 
+... содержимое h-файла
+
+#endif
+
+2. Файл должен содержать только обявления функций, а не их реализацию
+
+3. Отступы !!!
+*/
+
+
+/*=
+ * Не сразу понял, что эта функция должна делать. Название median -- явно неудачное,
+ * поскольку в мат. статистике имеет немного отличную семантику. Лучше назвать middle.
+ */
   // median function, search for middle among 3 numbers
   int median(int one, int two, int three) 
   {
+      /*=
+       * Этот код можно (и нужно!) сократить.
+       * Используй std::max и std::min
+       */
     int mx, mn;
     if (one > two) {
       mx = one; 
@@ -22,14 +45,29 @@
     return one + two + three - mn - mx;
   }
 
+  /*=
+   * Вектора нужно передавать по ссылке, а не через указатели
+   */
   // array sorting using quicksort algorithm with transposition indexes saving 
   void quicksort_indexes(std::vector< int > *sorted_array, int first,
                int last, std::vector< int > *array_with_indexes)
   {
+      /*=
+       * 1. Объявления переменных -- отдельными инструкциями
+       * 2. Индекс, используемый в std::vector -- это size_t, а не int
+       * 3. В целях безопасности нужно использовать vector::at() вместо оператора [],
+       *    поскольку в циклах нет явной проверки на выход за границы массива
+       */
       int indexA = first, indexB = last, x = 0, val = 0;
       x = median((*sorted_array)[first], (*sorted_array)[(first + last) / 2],
                                                             (*sorted_array)[last]);
       do {
+          /*=
+           * 1. Писать цикл в одну строку -- очень плохо, все отладчики при пошаговом выполнении
+           * будут выполнять две инструкции за один шаг
+           * 2. Два цикла while -- очень опасные, поскольку явно не проверяют выход
+           * за границы вектора
+           */
           while ((*sorted_array)[indexA] < x) indexA++;
           while ((*sorted_array)[indexB] > x) indexB--;
   
@@ -54,6 +92,11 @@
           quicksort_indexes(sorted_array, first, indexB, array_with_indexes);
   }
 
+  /*=
+   * Код этой функции дублирует функцию quicksort_indexes.
+   * Нужно оставить только quicksort_indexes, сделав дополнительные параметры
+   * опциональными
+   */
   // array sorting with quicksort algorithm
   void quicksort(std::vector< int > *sorted_array, int first,
                int last)
@@ -83,6 +126,12 @@
           quicksort(sorted_array, first, indexB);
   }
    
+  /*=
+   * 1. Сравнение int >= double фактически работает как static_cast<double>(int) > double.
+   * Строгое равенство при этом неочевидно ввиду особенностей внутреннего представления (см. IEEE 754-2008)
+   * От использования double нужно отказаться.
+   * 2. Передавать копию вектора -- слишком затратно. Нужно передавать const reference
+   */
   int binary_search(double search_value, std::vector< int > array, int from, int to)
   {
     int extremum = 0;
