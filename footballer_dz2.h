718b88f0b941d00aca5b48e8cb31cbb0c2dcaf97
Victor Yacovlev <v.yacovlev@gmail.com>
2014-12-13 00:52:49 +0300

    Update footballer_dz2.h
    Last (?) iteration on this file

--- footballer_dz2.h
+++ footballer_dz2.h
@@ -1,13 +1,29 @@
 
 #ifndef FOOTBALLER_DZ2_H
+/*== 
+Опечатка. После define должно быть то же имя, что и после ifndef
+*/
+
 #define FOOTBALLER_DZ2_
 
+/*==
+Для всех функций нужно объявить модификатор extern, иначе их нельзя будет использовать в другом модуле.
+Это необходимо только в .h файле, в .cpp -- не обязательно.
+*/
 
         void read(std::vector<int> &sorted_array);
         // middle function, search for middle among 3 numbers
         int middle(int one, int two, int three); 
 
         // array sorting using quicksort algorithm with transposition indexes saving
+        
+        /*==
+        Плохое решение для defult.
+        Каждый модуль (aka .cpp-файл), который будет включать в себя этот заголовок,
+        будет создавать локальную копию вектора.
+        Для out-параметра лучше не объявлять значение по умолчанию, либо использовать 
+        передачу по указателю (*), а не по ссылке (&), и использовать значение по умолчанию nullptr
+        */
         static std::vector<int> defult;
         void quicksort_indexes(std::vector<int> &sorted_array, int first,
                                     int last, std::vector<int> &array_with_indexes = defult);
